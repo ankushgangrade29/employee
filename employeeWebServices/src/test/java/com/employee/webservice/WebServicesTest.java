37df8dedaade9648d4a0d79e45006308e6c2b5fa
Ankush Gangrade <ankush.gangrade@worldpay.com>
2017-12-27 16:19:22 +0530

    Added test web service class

---
+++ employeeWebServices/src/test/java/com/employee/webservice/WebServicesTest.java
@@ -0,0 +1,32 @@
+package com.employee.webservice;
+
+import java.net.URI;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.jersey.client.ClientConfig;
+
+public class WebServicesTest {
+	public static void main(String[] args) {
+		ClientConfig config = new ClientConfig();
+
+		Client client = ClientBuilder.newClient(config);
+
+		WebTarget target = client.target(getBaseURI());
+
+		String response = target.path("api").path("getName")
+				.queryParam("name", "Ankush").request()
+				.accept(MediaType.APPLICATION_JSON).get(String.class)
+				.toString();
+		System.out.println(response);
+	}
+
+	private static URI getBaseURI() {
+		return UriBuilder.fromUri("http://localhost:18080/employee")
+				.build();
+	}
+}
